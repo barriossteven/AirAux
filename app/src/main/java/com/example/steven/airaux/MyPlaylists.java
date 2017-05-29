c759922a37d17894354efb13f315a01b7bf050d0
barriossteven <barriossteven@gmail.com>
2017-05-28 23:47:26 -0400

    files

---
+++ app/src/main/java/com/example/steven/airaux/MyPlaylists.java
@@ -0,0 +1,13 @@
+package com.example.steven.airaux;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPlaylists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_playlists);
+    }
+}
